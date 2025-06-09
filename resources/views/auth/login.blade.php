8143a06040aa73975b4a0e3714bf463645a92dc2
SoSyis <yaskurkurnia@gmail.com>
2025-06-09 19:01:49 +0700

    perubahan login/register page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,19 +2,36 @@
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+<!-- Section: Design Block -->
+<section class="">
+  <!-- Jumbotron -->
+  <div class="px-4 py-5 px-md-5 text-center text-lg-start" style="background-color: hsl(0, 0%, 96%)">
+    <div class="container">
+      <div class="row gx-lg-5 align-items-center">
+        <div class="col-lg-6 mb-5 mb-lg-0">
+          <h1 class="my-5 display-3 fw-bold ls-tight">
+            Perpustakaan Terbaik <br />
+            <span class="text-primary">Dan Terlengkap</span>
+          </h1>
+          <p style="color: hsl(217, 10%, 50.8%)">
+            Ini adalah perpustakaan terbaik dan terlengkap di seluruh indonesia, kalau ada buku yang tidak ada silahkan hubungi admin untuk penambahan bukunya. Terimakasih sudah menggunakan perpustakaan online ini.
+          </p>
         </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+        <div class="col-lg-6 mb-5 mb-lg-0">
+          <div class="card">
+            <div class="card-body py-5 px-md-5">
+              <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <!-- Email input -->
+                <div data-mdb-input-init class="form-outline mb-4">
+                  <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                </div>
+
+                <!-- Password input -->
+                <x-input-label for="password" :value="__('Password')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
@@ -22,26 +39,31 @@
                             required autocomplete="current-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
+                </div>
+                <!-- Submit button -->
+                <button type="submit"  class="btn btn-primary mb-2">
+                  {{ __('Log in') }}
+                </button>
+<h1 class="fw-bold text-center"><span>Jika belum punya akun silahkan register dahulu.</span>
+          </h1>
+                <!-- Register buttons -->
+                  <a href="{{ route('register') }}"  class="btn btn-primary btn-block mb-2 mt-2">
+                  Register
                 </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
+                                <h1 class="fw-bold text-center"><span>Atau kembali ke halaman utama</span>
+          </h1>
+                <!-- back buttons -->
+                  <a href=".."  class="btn btn-primary btn-block mb-4 mt-2">
+                  Home
+                </a>
+              </form>
+            </div>
+          </div>
         </div>
-    </form>
+      </div>
+    </div>
+  </div>
+  <!-- Jumbotron -->
+</section>
+<!-- Section: Design Block -->
 </x-guest-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,23 +1,41 @@
 <x-guest-layout>
+<!-- Section: Design Block -->
+<section class="">
+  <!-- Jumbotron -->
+  <div class="px-4 py-5 px-md-5 text-center text-lg-start" style="background-color: hsl(0, 0%, 96%)">
+    <div class="container">
+      <div class="row gx-lg-5 align-items-center">
+        <div class="col-lg-6 mb-5 mb-lg-0">
+          <h1 class="my-5 display-3 fw-bold ls-tight">
+            Perpustakaan Terbaik <br />
+            <span class="text-primary">Dan Terlengkap</span>
+          </h1>
+          <p style="color: hsl(217, 10%, 50.8%)">
+            Ini adalah perpustakaan terbaik dan terlengkap di seluruh indonesia, kalau ada buku yang tidak ada silahkan hubungi admin untuk penambahan bukunya. Terimakasih sudah menggunakan perpustakaan online ini.
+          </p>
+        </div>
+
+        <div class="col-lg-6 mb-5 mb-lg-0">
+          <div class="card">
+            <div class="card-body py-5 px-md-5">
     <form method="POST" action="{{ route('register') }}">
         @csrf
-
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
+                <!-- Name input -->
+                <div data-mdb-input-init class="form-outline mb-4">
+                  <x-input-label for="name" :value="__('Name')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
+                </div>
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
+                <!-- Email input -->
+                <div data-mdb-input-init class="form-outline mb-4">
+                  <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+                </div>
 
-        <!-- Password -->
-        <div class="mt-4">
+                <!-- Password input -->
+                <div data-mdb-input-init class="form-outline mb-4">
             <x-input-label for="password" :value="__('Password')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
@@ -26,10 +44,9 @@
                             required autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+                </div>
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
+                <!-- Password 2 input -->
             <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
@@ -37,16 +54,32 @@
                             name="password_confirmation" required autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                </div>
+                
+                <!-- Submit button -->
+                <button type="submit" class="btn btn-primary btn-block mb-2">
+                  {{ __('Register') }}
+                </button>
+                <h1 class="fw-bold text-center"><span>Jika sudah punya akun silahkan langsung login.</span>
+          </h1>
+                <!-- Register buttons -->
+                  <a href="{{ route('login') }}"  class="btn btn-primary btn-block mb-2 mt-2">
+                  Login
+                </a>
+                <h1 class="fw-bold text-center"><span>Atau kembali ke halaman utama</span>
+          </h1>
+                <!-- back buttons -->
+                  <a href=".."  class="btn btn-primary btn-block mb-4 mt-2">
+                  Home
+                </a>
+              </form>
+            </div>
+          </div>
         </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
+      </div>
+    </div>
+  </div>
+  <!-- Jumbotron -->
+</section>
+<!-- Section: Design Block -->
 </x-guest-layout>
